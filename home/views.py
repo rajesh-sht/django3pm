6818682b74cd665adad9fd916d95d4984a8277dd
rajesh-sht <shrestharajesh.sr@gmail.com>
2023-01-29 16:13:41 +0545

    work in templates for looping and create views in views.py

--- home/views.py
+++ home/views.py
@@ -4,11 +4,16 @@ from .models import *
 def home(request):
     views = {}
     views['services'] = Service.objects.all()
+    views['feedbacks'] = Feedback.objects.all()
     return render(request, 'index.html',views)
 def about(request):
-    return render(request, 'about.html')
+    views = {}
+    views['feedbacks'] = Feedback.objects.all()
+    return render(request, 'about.html',views)
 def contact(request):
-    return render(request, 'contact.html')
+    views = {}
+    views['informations'] = Information.objects.all()
+    return render(request, 'contact.html', views)
 def portfolio(request):
     return render(request, 'portfolio.html')
 def price(request):
